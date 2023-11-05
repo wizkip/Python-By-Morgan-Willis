b1a1529128fdbdd20aba8d452b76faa74fe68201
Wesley Kipkemoi Sang <40331006+wizkip@users.noreply.github.com>
2023-11-05 07:24:48 +0000

    more actions

--- lists-indexing/Lindex.py
+++ lists-indexing/Lindex.py
@@ -4,9 +4,13 @@ animals = ["cow", "donkey", "rabbit", "horse"]
 animals.append("Leopard")
 animals.append("Lion")
 print(animals)
-#to retrieve items by indices
+
+#to retrieve items by indices and slice
 print(animals[0:3])
 print(animals[2])
+print(animals[:3]) #Print from item 0 to 2 but not including 3
+print(animals[::2]) #Jump by 1
+print(animals[::1]) #print all the items
 
 #To perform additions to the List
 prime = [2,4,5,6,8,7]
