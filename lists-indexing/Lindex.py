97fedd3ba8fe36bf47d416bbad40c41c6f6ffa92
Wesley Kipkemoi Sang <40331006+wizkip@users.noreply.github.com>
2023-11-05 07:29:05 +0000

    finer details including step jumps

--- lists-indexing/Lindex.py
+++ lists-indexing/Lindex.py
@@ -9,8 +9,10 @@ print(animals)
 print(animals[0:3])
 print(animals[2])
 print(animals[:3]) #Print from item 0 to 2 but not including 3
-print(animals[::2]) #Jump by 1
+print(animals[::2]) #step Jump by 1
 print(animals[::1]) #print all the items
+print(animals[::-1]) #print the whole list backwards
+print(animals[::-2]) #print the list backwards jumping by 1
 
 #To perform additions to the List
 prime = [2,4,5,6,8,7]
