f9c4b619e4ca8a43b61e69f512eb5930b10533ab
Wesley Kipkemoi Sang <40331006+wizkip@users.noreply.github.com>
2023-11-05 07:18:57 +0000

    general list ops

---
+++ lists-indexing/Lindex.py
@@ -0,0 +1,15 @@
+animals = ["cow", "donkey", "rabbit", "horse"]
+
+#to append to the list
+animals.append("Leopard")
+animals.append("Lion")
+print(animals)
+#to retrieve items by indices
+print(animals[0:3])
+print(animals[2])
+
+#To perform additions to the List
+prime = [2,4,5,6,8,7]
+
+print(sum(prime))
+
